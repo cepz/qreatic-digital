6576f034843d3bd29a806e585067d1e28fe30172
GMV <gmv@MangCep-MacBook-Pro.local>
2020-05-01 02:57:51 +0700

    optimized develop build use yarn build

---
+++ build/precache-manifest.215a10c27200dbda0f04d9e88b7c97f8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c170e765237337247b66f0043d5aa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc64c3c066fcdc08a36",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6bf4d72ee3a69ae7aadb",
+    "url": "/static/css/main.62a958e6.chunk.css"
+  },
+  {
+    "revision": "fbc64c3c066fcdc08a36",
+    "url": "/static/js/2.9822620d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9822620d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf4d72ee3a69ae7aadb",
+    "url": "/static/js/main.c27a6d63.chunk.js"
+  },
+  {
+    "revision": "a96cc94860f22146e1cd",
+    "url": "/static/js/runtime-main.6b19655f.js"
+  },
+  {
+    "revision": "45c2c801b0620effaf72c125cbca9c2b",
+    "url": "/static/media/1.45c2c801.jpg"
+  },
+  {
+    "revision": "9d478764c37039cbad35663214a04a13",
+    "url": "/static/media/2.9d478764.jpg"
+  },
+  {
+    "revision": "fe3d61b2ab1c17ad8309d68e6e742433",
+    "url": "/static/media/3.fe3d61b2.jpg"
+  },
+  {
+    "revision": "cb17b6c152e798d5b6b3b2a84b3800af",
+    "url": "/static/media/4.cb17b6c1.jpg"
+  },
+  {
+    "revision": "d4c86e4e816f764701d969d75c4c479f",
+    "url": "/static/media/logo.d4c86e4e.jpg"
+  },
+  {
+    "revision": "d4c86e4e816f764701d969d75c4c479f",
+    "url": "/static/media/profile.d4c86e4e.jpg"
+  }
+]);
